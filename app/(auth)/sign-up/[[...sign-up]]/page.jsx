8b33bc92dc35b10f483882c26f444aaa954b7a66
Sugata <sarkarsugata57@gmail.com>
2024-10-06 17:35:45 +0530

    setting up db, login page and font

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,5 +1,14 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-    return <SignUp />
+    return(
+        <div className='grid grid-cols-1 md:grid-cols-2'>
+            <div>
+                <Image src={'/login.jpg'} alt={'login'} width={500} height={500} className='w-full object-contain'/>
+            </div>
+            <div className='flex justify-center items-center h-screen'>
+                <SignUp />
+            </div>
+        </div>
+    )
 }
--- app/provider.js
+++ app/provider.js
@@ -1,6 +1,27 @@
-import React from 'react'
+"use client"
+import { db } from '@/configs/db';
+import { Users } from '@/configs/schema';
+import { useUser } from '@clerk/nextjs';
+import { eq } from 'drizzle-orm';
+import React, { useEffect } from 'react'
 
 function Provider({children}) {
+    const {user} = useUser();
+    useEffect(() => {
+        user && isNewUser();
+    }, [user])
+    const isNewUser = async() => {
+        const result =await db.select().from(Users)
+        .where(eq(Users.email,user?.primaryEmailAddress?.emailAddress));
+        if(result.length === 0){
+            await db.insert(Users).values({
+                name: user.fullName,
+                email: user?.primaryEmailAddress?.emailAddress,
+                imageUrl: user?.imageUrl,
+                //subscription: false
+            })
+        }
+    }
     return (
         <div>
             {children}
