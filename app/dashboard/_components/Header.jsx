4deb30dc9a4f56ea52c5f694441bce7f8b75f0b9
TDNM88 <167914317+TDNM88@users.noreply.github.com>
2024-10-25 18:49:40 +0700

    Update Header.jsx

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
         <div className='p-3 px-5 items-center justify-between flex shadow-md'>
             <div className='flex items-center gap-3'>
                 <Image src={'/logo.jpeg'} alt={'logo'} width={40} height={40}/>
-                <h2 className='font-bold text-xl'>ClipAi</h2>
+                <h2 className='font-bold text-xl'>Video Ai</h2>
             </div>
             <div className='flex items-center gap-3'>
                 <Button>Dashboard</Button>
